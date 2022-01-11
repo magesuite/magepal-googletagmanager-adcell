f4a552ef117f1e8a8978fe1df4363b29ea85ec3f
Piotr Matras <piotr.matras@creativestyle.pl>
2022-01-11 07:47:28 +0100

    Fix integration tests

--- Test/Integration/Observer/AddSearchDataTest.php
+++ Test/Integration/Observer/AddSearchDataTest.php
@@ -40,10 +40,10 @@ class AddSearchDataTest extends \Magento\TestFramework\TestCase\AbstractControll
         $this->dispatch('/catalogsearch/result/index');
         $body = $this->getResponse()->getBody();
         $expectedString = $this->serializer->serialize([
-            'event' => 'searchPage',
             'search_term' => 'simple',
             'productIds' => '333',
-            'productSeparator' => \MageSuite\MagePalGoogleTagManagerAdcell\Model\DataLayer\DataProviderInterface::PRODUCT_SEPARATOR
+            'productSeparator' => \MageSuite\MagePalGoogleTagManagerAdcell\Model\DataLayer\DataProviderInterface::PRODUCT_SEPARATOR,
+            'event' => 'searchPage'
         ]);
 
         $this->assertStringContainsString($expectedString, $body);
