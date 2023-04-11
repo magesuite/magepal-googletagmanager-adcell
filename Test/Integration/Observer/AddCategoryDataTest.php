2944ccea52f72221b3b2ad5818b5761d182051ff
Blazej Doleska <blazej.doleska@creativestyle.pl>
2023-04-11 16:56:15 +0200

    added Magento version compare for tests

--- Test/Integration/Observer/AddCategoryDataTest.php
+++ Test/Integration/Observer/AddCategoryDataTest.php
@@ -10,10 +10,15 @@ class AddCategoryDataTest extends \Magento\TestFramework\TestCase\AbstractContro
      */
     protected $serializer;
 
+    /**
+     * @var \Magento\Framework\App\ProductMetadataInterface
+     */
+    protected $productMetadata;
     protected function setUp(): void
     {
         parent::setUp();
         $this->serializer = $this->_objectManager->get(\Magento\Framework\Serialize\SerializerInterface::class);
+        $this->productMetadata = $this->_objectManager->get(\Magento\Framework\App\ProductMetadataInterface::class);
     }
 
     /**
@@ -27,16 +32,30 @@ class AddCategoryDataTest extends \Magento\TestFramework\TestCase\AbstractContro
         $this->getRequest()->setParam('id', 333);
         $this->dispatch('/catalog/category/view');
         $body = $this->getResponse()->getBody();
+
         $expectedString = $this->serializer->serialize([
             'event' => 'categoryPage',
             'category' => [
                 'id' => '333',
                 'name' => 'Category 1',
-                'path' => 'Category 1',
+                'path' => '',
                 'productIds' => '333',
                 'productSeparator' => \MageSuite\MagePalGoogleTagManagerAdcell\Model\DataLayer\DataProviderInterface::PRODUCT_SEPARATOR
             ]
         ]);
+
+        if (version_compare($this->productMetadata->getVersion(), '2.4.6') >= 0) {
+            $expectedString = $this->serializer->serialize([
+                'event' => 'categoryPage',
+                'category' => [
+                    'id' => '333',
+                    'name' => 'Category 1',
+                    'path' => 'Category 1',
+                    'productIds' => '333',
+                    'productSeparator' => \MageSuite\MagePalGoogleTagManagerAdcell\Model\DataLayer\DataProviderInterface::PRODUCT_SEPARATOR
+                ]
+            ]);
+        }
 
         $this->assertStringContainsString($expectedString, $body);
     }
